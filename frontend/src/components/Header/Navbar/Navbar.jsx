1c02b4f4fb011c24324dd46d811262a873999620
khennier <78939009+khennier@users.noreply.github.com>
2024-06-08 10:07:04 -0300

    actualizar

---
+++ frontend/src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Products</a></li>
+      <li><a href="#">Articles</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Gallery</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
